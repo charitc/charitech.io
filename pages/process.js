a1b5d216b61c68a7a369abacc53a78c7553a5a76
Max Eise <maximilian.eise@gmail.com>
2021-02-07 20:58:34 +0100

    Process Page

---
+++ pages/process.js
@@ -0,0 +1,48 @@
+import React, { useEffect } from "react";
+import AOS from "aos";
+
+import Nav from "../components/Nav";
+import Illustration from "../partials/Illustration";
+
+import ProcessHero from "../partials/ProcessHero";
+import Stats from "../partials/Stats";
+import ProcessZigZaq from "../partials/ProcessZiqZaq";
+import IconBlock from "../partials/IconBlock";
+import CTA from "../partials/ProcessCTA";
+import Footer from "../components/Footer";
+
+export default function Process() {
+  useEffect(() => {
+    AOS.init({
+      once: true,
+      disable: "phone",
+      duration: 600,
+      easing: "ease-out-sine",
+    });
+  });
+  return (
+    <div className="flex flex-col min-h-screen overflow-hidden">
+      {/*  Site header */}
+      <Nav />
+
+      {/*  Page content */}
+      <main className="flex-grow">
+        {/*  Page illustration */}
+        <div className="relative max-w-6xl mx-auto h-0 pointer-events-none" aria-hidden="true">
+          <Illustration />
+        </div>
+
+        {/*  Page sections */}
+        <ProcessHero />
+        <Stats />
+        <ProcessZigZaq />
+        <IconBlock />
+        {/* <CaseStudies /> */}
+        <CTA />
+      </main>
+
+      {/*  Site footer */}
+      <Footer />
+    </div>
+  );
+}
