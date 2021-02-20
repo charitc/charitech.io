4508c992f382bc171b772c9194e1df5afaa40ad6
Max Eise <maximilian.eise@gmail.com>
2021-02-20 19:05:22 +0100

    Authentication preperation

--- pages/auth_status.js
+++ pages/auth_status.js
@@ -4,8 +4,8 @@ export default function Component() {
   const [session, loading] = useSession();
 
   if (session) {
-    return <p>Signed in as {session.user.accounts}</p>;
     console.log(session);
+    return <p>Signed in as {session.user.accounts}</p>;
   }
 
   return <a href="/api/auth/signin">Sign in</a>;
